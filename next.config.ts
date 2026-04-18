21b2bfff74cff3878e47d1ca953b3bfc048a28af
Mayur Bhil <142144848+Mayur-Bhil@users.noreply.github.com>
2026-04-18 16:53:18 +0530

    Add TypeScript ignoreBuildErrors option

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     '10.157.23.91',
   ],
+   typescript: {
+    ignoreBuildErrors: true, // ← add this line
+  },
 };
 
 export default nextConfig;
